247a2627ec519ba7b1247ac6c4ec6cb8ce66a84d
Luis Mendes <machadoluismendes@gmail.com>
2021-09-09 14:44:33 -0300

    Authenticate python bitbucket

---
+++ examples/authenticate-python-bitbucket.py
@@ -0,0 +1,31 @@
+# https://auth0.com/authenticate/python/bitbucket/
+
+# Controllers API
+
+# This doesn't need authentication
+@APP.route("/api/public")
+@cross_origin(headers=["Content-Type", "Authorization"])
+def public():
+    response = "Hello from a public endpoint! You don't need to be authenticated to see this."
+    return jsonify(message=response)
+
+# This needs authentication
+@APP.route("/api/private")
+@cross_origin(headers=["Content-Type", "Authorization"])
+@requires_auth
+def private():
+    response = "Hello from a private endpoint! You need to be authenticated to see this."
+    return jsonify(message=response)
+
+# This needs authorization
+@APP.route("/api/private-scoped")
+@cross_origin(headers=["Content-Type", "Authorization"])
+@requires_auth
+def private_scoped():
+    if requires_scope("read:messages"):
+        response = "Hello from a private endpoint! You need to be authenticated and have a scope of read:messages to see this."
+        return jsonify(message=response)
+    raise AuthError({
+        "code": "Unauthorized",
+        "description": "You don't have access to this resource"
+    }, 403)
